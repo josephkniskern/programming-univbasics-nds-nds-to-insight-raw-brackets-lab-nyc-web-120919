22b5559bfdee2eda48d5c87dd9d5ffa1228c76de
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-05 21:15:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+  
+  print directors_database
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
